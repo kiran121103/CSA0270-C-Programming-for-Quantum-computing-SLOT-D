ab233fb9c95f016efbccd8f6f2888d95ee9d95d2
kiran121103 <121809863+kiran121103@users.noreply.github.com>
2023-04-11 21:43:15 +0530

    Add files via upload

---
+++ day-1 sum of  n natural numbers-7.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int n, sum;
+printf("Enter the value of n: ");
+scanf("%d", &n);
+sum = (n * (n + 1)) / 2;
+printf("Sum of first %d natural numbers is %d", n, sum);
+return 0;
+}
+
